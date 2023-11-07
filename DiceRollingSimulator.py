a66302bf27fd488643abf932f17795030482e6c3
Geraldi Gunawan <geraldigunawan23@gmail.com>
2023-11-08 09:34:48 +1100

    Update DiceRollingSimulator.py
    test update after a while

--- DiceRollingSimulator.py
+++ DiceRollingSimulator.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """
-The Goal: Like the title suggests, this project involves writing a program 
-that simulates rolling dice. When the program runs, it will randomly choose a 
-number between 1 and 6. (Or whatever other integer you prefer — 
-the number of sides on the die is up to you.) The program will print what that number is. 
-It should then ask you if you’d like to roll again. For this project, you’ll need to set 
-the min and max number that your dice can produce. For the average die, that means a minimum of 1 
-and a maximum of 6. After dice is rolled, the user needs to guess what that number is. 
-(In other words, the user needs to be able to input information.) 
-If the user’s guess is wrong, the program should return some sort of indication as to how wrong 
-(e.g. The number is too high or too low). If the user guesses correctly, a positive indication 
-should appear"""
+The Goal: Like the title suggests, this project involves writing a program
+that simulates rolling dice. When the program runs, it will randomly choose a
+number between 1 and 6. (Or whatever other integer you prefer —
+the number of sides on the die is up to you.) The program will print what that number is.
+It should then ask you if you’d like to roll again. For this project, you’ll need to set
+the min and max number that your dice can produce. For the average die, that means a minimum of 1
+and a maximum of 6. After dice is rolled, the user needs to guess what that number is.
+(In other words, the user needs to be able to input information.)
+If the user’s guess is wrong, the program should return some sort of indication as to how wrong
+(e.g. The number is too high or too low). If the user guesses correctly, a positive indication
+should appear."""
 
 import random
 
@@ -33,7 +33,7 @@ def checkNumber(guess_number, actual_number):
         print ("your number is a bit high");
     else:
         return 0
-    
+
 def checkInput(a):
     a = a.lower();
     while (a not in valid_answers):
@@ -60,7 +60,6 @@ while(answer in valid_yes_answers):
         print ("You've run out of lives. You lose.")
     answer = input("Would you like to play again? (Y/N)");
     answer = checkInput(answer)
-        
+
 print ("Thanks, bye-bye");
 #%%
-                
